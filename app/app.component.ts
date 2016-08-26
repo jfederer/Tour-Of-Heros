8bf476aa4ebe6521ed0fd1e215f65f70bdaeb4ac
jfederer <jfederer@usgs.gov>
2016-08-26 17:09:54 -0500

    Service used and works with mocks

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,25 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import {Hero} from './hero';
-
-
-const HEROES: Hero[] = [
-  { id: 11, name: 'Mr. Nice' },
-  { id: 12, name: 'Narco' },
-  { id: 13, name: 'Bombasto' },
-  { id: 14, name: 'Celeritas' },
-  { id: 15, name: 'Magneta' },
-  { id: 16, name: 'RubberMan' },
-  { id: 17, name: 'Dynama' },
-  { id: 18, name: 'Dr IQ' },
-  { id: 19, name: 'Magma' },
-  { id: 20, name: 'Tornado' }
-];
+import { HeroService } from './hero.service';
 
 @Component({
   selector: 'my-app',
+  providers: [HeroService],
   template: `
    <h1>{{title}}</h1>
-   <h2>My Heroes</h2>
+   <h2>My Heroes </h2>
    <ul class="heroes">
      <li *ngFor="let hero of heroes"
        [class.selected]="hero === selectedHero"
@@ -82,12 +70,26 @@ const HEROES: Hero[] = [
 		}
 		`]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+	constructor(private heroService: HeroService) { }
+
 	title : string = "Tour of Heroes";
-	heroes : Hero[] = HEROES;
+	heroes : Hero[];
 	selectedHero:Hero;
+
+
+
+	getHeroes(): void {
+  		this.heroes = this.heroService.getHeroes();
+	}
 
 	onSelect(hero:Hero):void {
 		this.selectedHero = hero;
 	}
+
+	ngOnInit(): void {
+		this.getHeroes();
+	}
+
+
 }
