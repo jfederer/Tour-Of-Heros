01dc68c3027c3e058ab6d45d90e90163af9b2f58
jfederer <jfederer@usgs.gov>
2016-08-29 14:07:29 -0500

    Added setTimeout promise to the getHeros function

--- app/hero.service.ts
+++ app/hero.service.ts
@@ -4,7 +4,20 @@ import { HEROES } from './mock-heroes';
 
 @Injectable()
 export class HeroService {
+
+
 	getHeroes(): Promise<Hero[]> {
-	  return Promise.resolve(HEROES);
+		var prom : Promise<Hero[]> = new Promise( (resolve)=> {
+			setTimeout(() => {
+				resolve(HEROES);
+			},750);
+		});
+		return prom;
+
+		//
+		// return new Promise<Hero[]>(resolve =>
+	    //   setTimeout(resolve, 2000)) // delay 2 seconds
+	    //   .then(() => this.getHeroes());
+	// 	return Promise.resolve(HEROES);
 	}
 }
