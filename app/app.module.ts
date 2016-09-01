6fc9a2d8abc851dedb985116ca6b1681b7c64b08
jfederer <jfederer@usgs.gov>
2016-09-01 14:41:35 -0500

    Set up in memory web api module

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,6 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
+import { HttpModule  }   from '@angular/http';
+
+// Imports for loading & configuring the in-memory web api
+import { InMemoryWebApiModule } from 'angular2-in-memory-web-api';
+import { InMemoryDataService }  from './in-memory-data.service';
+
 import { AppComponent } from './app.component';
 import { HeroDetailComponent } from './hero-detail.component';
 import { HeroesComponent } from './heroes.component';
@@ -12,7 +18,9 @@ import { DashboardComponent } from './dashboard.component';
   	imports: [
 	  	BrowserModule,
 	  	FormsModule,
-  		routing],
+		HttpModule,
+  		routing,
+		InMemoryWebApiModule.forRoot(InMemoryDataService)],
   	providers: [
 		HeroService],
   	declarations: [
